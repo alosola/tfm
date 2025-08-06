Fix normalization

lib/Stokes.py
@@ -32,24 +32,11 @@ class Stokes:
         print("Saving wavelength calibration to", self.name, "object")
         self.wave_array = wave_array
 
-        
-    def normalize(self):
-        # Normalize data by dividing by the "quiet sun" value at each wavelength
-        # if hasattr(self, 'mean_quiet'):
-        #     print("Normalizing", self.name, "data with quiet sun profile", self.mean_quiet.mean())
-        #     self.data_n = (self.data + np.abs(self.data.min())) / (self.mean_quiet.mean() + np.abs(self.data.min()))
-        # if hasattr(self, 'mean_quiet'):
-        #     print("Normalizing", self.name, "data with Intensity quiet sun profile", 9326.845)
-        #     self.data_n = (self.data + np.abs(self.data.min())) / (9326.845 + np.abs(self.data.min()))
-        # else:
-        #     # Normalizing data with value xnormalized = (x - xminimum) / range of x
-        #     print("Normalizing data for", self.name, "and saving to object")
-        #     self.data_n = (self.data - self.data.min()) / (self.data.max() - self.data.min())
 
-        # Normalizing data with value xnormalized = (x - xminimum) / range of x
-        print("Normalizing data for", self.name, "and saving to object")
-        self.data_n = (self.data - self.data.min()) / (self.data.max() - self.data.min())
-
+    def normalize(self, I_cont):
+        # Normalizing data by dividing by the "quiet sun" Intensity continuum value 
+        print(f"Normalizing data for {self.name} with I continuum value {I_cont} and saving to object")
+        self.data_n = self.data / I_cont
         
 
     def mean_quiet_region(self, xmin, xmax, ymin, ymax):