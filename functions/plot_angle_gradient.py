db892e3094f9cc3460f80fa986c32ed0e7d09b42
asola <alondra.sola-molina@obspm.fr>
2025-10-01 16:53:05 +0200

    Advances on temp and velocity

--- functions/plot_angle_gradient.py
+++ functions/plot_angle_gradient.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-def plot_angle_gradient(data_radians, title=''):
+def plot_angle_gradient(data_radians, title='', colourmap='twilight'):
     # Plot angle with cyclic colormap
     fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(8, 8))
     ax.set_xlabel('X axis (array index)')
     ax.set_ylabel('Y axis (array index)')
     ax.set_title(title)
 
-    img = ax.imshow(np.rad2deg(data_radians), cmap='twilight')
+    img = ax.imshow(np.rad2deg(data_radians), cmap=colourmap)
 
     divider = make_axes_locatable(ax)
     cax = divider.append_axes("right", size="5%", pad=0.05)
